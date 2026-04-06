4836ea35540a01b8a256ae0d17ea2be7932702ca
blarive <blarive@users.noreply.github.com>
2026-04-06 13:03:05 +0000

    deploy: c8aa6d1a4bf05aa47d5b7ea16dc15cc269dad168

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 
 // ⚠️  CE NOM CHANGE À CHAQUE DÉPLOIEMENT (via GitHub Actions ou manuellement)
 //     Changer ce nom invalide l'ancien cache et force le téléchargement
-const CACHE_NAME = 'carnet-peche-v20260406124823';
+const CACHE_NAME = 'carnet-peche-v20260406130305';
 
 const STATIC_ASSETS = [
   './',
