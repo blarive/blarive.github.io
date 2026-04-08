4056190c4113b7f79e885d5bfcfd6fd7d6f036e2
blarive <81332714+blarive@users.noreply.github.com>
2026-04-08 18:18:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 
 // ⚠️  CE NOM CHANGE À CHAQUE DÉPLOIEMENT (via GitHub Actions ou manuellement)
 //     Changer ce nom invalide l'ancien cache et force le téléchargement
-const CACHE_NAME = 'carnet-peche-v202604060717';
+const CACHE_NAME = 'carnet-peche-v2026040608';
 
 const STATIC_ASSETS = [
   './',
